5fece9ff1a7da66f85b5eac3eb8dc5779c932d54
Mark Melnik <markemelnik@gmail.com>
2023-05-11 15:42:21 -0400

    initial commit; react project setup

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <h1>Shopping Cart</h1>;
+};
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createGlobalStyle } from "styled-components";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const GlobalStyle = createGlobalStyle`
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+
+`;
+
+root.render(
+  <>
+    <GlobalStyle />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </>
+);
